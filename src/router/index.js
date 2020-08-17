74e9f02dc27e06d8c740bb5a56ed5945d8f44bce
xieguanzheng <2017447043@qq.com>
2020-08-17 09:20:14 +0800

    two

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Index from '@/components/Index/Index'
+import Cate from '@/components/Index/Cate'
+import News from '@/components/Index/News'
+import Search from '@/components/Index/Search'
+import Full from '@/components/Index/Full'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Index',
+      name: 'Index',
+      component: Index
+    },{
+      path: '/Cate' ,
+      name: 'Cate',
+      component: Cate
+    },{
+      path: '/News' ,
+      name: 'News',
+      component: News
+    },{
+      path: '/Search' ,
+      name: 'Search',
+      component: Search
+    },{
+      path: '/Full' ,
+      name: 'Full',
+      component: Full
     }
   ]
 })
